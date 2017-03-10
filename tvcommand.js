27e242e7a51691ba2c57c46654abd5514c256b3c
onok <onokje@gmail.com>
2017-03-10 22:27:29 +0100

    minor fixes in serial

--- tvcommand.js
+++ tvcommand.js
@@ -2,8 +2,8 @@ var SerialPort = require('serialport');
 
 module.exports = {
 
-    cmdOn: 'ka 0 1',
-    cmdOff: 'ka 0 0',
+    cmdOn: "ka 0 1\n",
+    cmdOff: "ka 0 0\n",
 
     on: function () {
         console.log("turn on TV");
@@ -16,15 +16,17 @@ module.exports = {
     },
 
     sendCommand: function (cmd) {
-        var port = new SerialPort("/dev/serial0", {baudrate: 9600}, function (err) {
+
+        var port = new SerialPort("/dev/ttyAMA0", {baudrate: 9600}, function (err) {
             if (err) {
                 return console.log('Error: ', err.message);
             }
-            port.write(cmd, function (err) {
-                if (err) {
-                    return console.log('Error on write: ', err.message);
-                }
-                console.log('message written');
+            port.write(cmd, function (err, results) {
+                if (err)
+                    console.log('err ' + err);
+                else
+                    console.log('results ' + results);
+                port.close();
             });
         });
     }
